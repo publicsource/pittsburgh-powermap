a610608833b49ef1528ae88d6db6a9fa43551630
jessica <jessica.mcinchak@gmail.com>
2020-11-04 20:50:49 +0100

    summing chart data by attributes

--- src/components/chart.js
+++ src/components/chart.js
@@ -2,8 +2,18 @@ import React from 'react'
 import Highcharts from 'highcharts'
 import HighchartsReact from 'highcharts-react-official'
 
+import _ from 'lodash'
+import { getDecade } from 'date-fns'
+
 const BarChart = ({ data }) => {
-    console.log(data)
+    let byRace = _.countBy(data.edges, 'node.data.Race');
+    let bySex = _.countBy(data.edges, 'node.data.Gender');
+
+    let dataWithDecades = data.edges.map(e => {
+        let d = e.node.data;
+        return ({ ...d, 'Decade': getDecade(new Date(d.Birthdate))})
+    });
+    let byDecade = _.countBy(dataWithDecades, 'Decade');
 
     const options = {
         title: {
